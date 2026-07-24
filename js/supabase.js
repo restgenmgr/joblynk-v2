f5eb815aaa77715073b8186db5a9df287e318fa7
Nigel Anthony Thomas <restgenmgr1952@gmail.com>
2026-07-24 21:03:56 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,34 +1,8 @@
-document.getElementById("registerForm").addEventListener("submit", async (e) => {
+const SUPABASE_URL = "https://mjpxtkqvdalpmcudfagn.supabase.co";
 
-    e.preventDefault();
+const SUPABASE_KEY = "sb_publishable_mnulkfbifdqxyQ5WT-9Tig_uPqTEdNB";
 
-    const fullName = document.getElementById("fullname").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    const { data, error } = await window.supabaseClient.auth.signUp({
-        email,
-        password,
-        options: {
-            data: {
-                full_name: fullName
-            }
-        }
-    });
-
-    if (error) {
-        alert(error.message);
-        return;
-    }
-
-    console.log(data);
-
-    if (data.user && data.session) {
-        alert("Registration successful. You are now logged in.");
-    } else if (data.user) {
-        alert("Registration successful.");
-    } else {
-        alert("Unknown response from Supabase.");
-    }
-
-});
+window.supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
