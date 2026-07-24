1d3785a0a6800669cee83c9016bfdf4b21219605
restgenmgr <restarantbarmanager@gmail.com>
2026-07-24 12:13:23 +0530

    Add candidate registration page

---
+++ js/register.js
@@ -0,0 +1,34 @@
+document.getElementById("registerForm").addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const fullName = document.getElementById("fullname").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const { data, error } = await supabase.auth.signUp({
+
+        email: email,
+        password: password,
+
+        options: {
+
+            data: {
+                full_name: fullName
+            }
+
+        }
+
+    });
+
+    if (error) {
+
+        alert(error.message);
+
+    } else {
+
+        alert("Registration Successful! Please check your email to verify your account.");
+
+    }
+
+});
