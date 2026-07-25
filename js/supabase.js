8e89332f47be30542e23d3073585dc3f03438714
Nigel Anthony Thomas <restgenmgr1952@gmail.com>
2026-07-25 09:06:39 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,7 @@
 const SUPABASE_URL = "https://mjpxtkqvdalpmcudfagn.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_mnulkfbifdqxyQ5WT-9Tig_uPqTEdNB";
+const SUPABASE_KEY =
+    "sb_publishable_mnulkfbifdqxyQ5WT-9Tig_uPqTEdNB";
 
 window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
