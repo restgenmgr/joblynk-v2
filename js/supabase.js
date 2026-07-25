61eb678496f1c6dea4eed3cf20342abae92d34df
Nigel Anthony Thomas <restgenmgr1952@gmail.com>
2026-07-25 19:54:48 +0530

    Refactor Supabase client initialization code

--- js/supabase.js
+++ js/supabase.js
@@ -5,6 +5,6 @@ const SUPABASE_ANON_KEY = "sb_publishable_LasHnUnDsJLQdMocHw0-OQ_lT3G6mnm";
 window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
-);
+); 
 
 console.log("Connected to:", SUPABASE_URL);
